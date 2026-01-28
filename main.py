81765dc64f1096fdaeb3076be7a8522e5c8c2bc5
Dina Metwalli <dina.moh657@gmail.com>
2026-01-28 09:48:36 +0000

    refactor: update main file

--- main.py
+++ main.py
@@ -10,19 +10,20 @@ def main():
     data = loader.load_data()
 
     # Multi-region training
-    loader = DataLoader(False, regions=['africa', 'asia', 'europe', 'arctic', 'antarctic',
-                                        'northAmerica', 'southAmerica', 'oceania'])
+    regions=['africa', 'asia', 'europe', 
+             'northAmerica', 'southAmerica', 'oceania']
+    loader = DataLoader(False, regions=regions)
     data = loader.load_data()
     
     # Initialise model
-    model = ClimateForecastingModel(data=data, seq_len=60, forecast_num=60)
+    model = ClimateForecastingModel(data=data, regions=regions, seq_len=60, forecast_num=60, model_file='regional_climate_lstm.pth', scaler_file='scaler.pkl')
     
     # Complete future forecast using a testing dataframe
     testing_loader = DataLoader(True, "csv", "Africa_Data.csv")
     testing_data = testing_loader.load_data()
     
     model.predict_future(dataset=testing_data, months_to_test=60, test_future=False)
-    model.save_model()
+    # model.save_model()
 
 if __name__ == "__main__":
     main()
