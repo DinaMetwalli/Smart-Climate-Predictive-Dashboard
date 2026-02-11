054dbf635bf459e17091654b8de0265b83b9617d
Dina Metwalli <dina.moh657@gmail.com>
2026-02-11 16:38:45 +0000

    fix(data): correct try except wrapping in file data validation

--- data/preparation/custom_data_retriever.py
+++ data/preparation/custom_data_retriever.py
@@ -29,33 +29,33 @@ class CustomDatasetRetriever:
             raise InvalidFileTypeError("Ivalid file type provided. Files must have CSV or NC extensions.")
 
     def validate_file_data(self, file, file_type):
-        try:
-            cols = ['Anomaly', 'Date', 'Region']
-            
+        try:    
             if file_type == "csv":
                 data = pd.read_csv(file)
             else:
                 ds = xr.open_dataset(file)
                 data = ds.to_dataframe().reset_index()
 
-            if set(data.columns.values) != set(cols):
-                raise IncompatibleDataError(
-                    "The provided dataset structure is incompatible. Please ensure the fields (Date, Anomaly, Region) are present in that order."
-                    )
-            
-            # Check all input data is of the expected type
-            if not data['Date'].map(lambda x: isinstance(x, int)).all():
-                raise IncompatibleDataError("Provided column 'Date' contains non-numeric values. Expected format: YYYYMM")
-            
-            if not data['Anomaly'].map(lambda x: isinstance(x, (int, float))).all():
-                raise IncompatibleDataError("Provided column 'Anomaly' contains non-numeric values.")
-            
-            if not data['Region'].map(lambda x: isinstance(x, str)).all():
-                raise IncompatibleDataError("Provided column 'Region' contains non-text values. Allowed values: Africa, Asia," \
-                " Europe, Northamerica, Southamerica, Oceania.")
-        
         except Exception:
             raise FileProcessingError("An error was encountered when opening the file.")
+
+        cols = ['Anomaly', 'Date', 'Region']
+        
+        if set(data.columns.values) != set(cols):
+            raise IncompatibleDataError(
+                "The provided dataset structure is incompatible. Please ensure the fields (Date, Anomaly, Region) are present in that order."
+                )
+        
+        # Check all input data is of the expected type
+        if not data['Date'].map(lambda x: isinstance(x, int)).all():
+            raise IncompatibleDataError("Provided column 'Date' contains non-numeric values. Expected format: YYYYMM")
+        
+        if not data['Anomaly'].map(lambda x: isinstance(x, (int, float))).all():
+            raise IncompatibleDataError("Provided column 'Anomaly' contains non-numeric values.")
+        
+        if not data['Region'].map(lambda x: isinstance(x, str)).all():
+            raise IncompatibleDataError("Provided column 'Region' contains non-text values. Allowed values: Africa, Asia," \
+            " Europe, Northamerica, Southamerica, Oceania.")
         
         return data
     
