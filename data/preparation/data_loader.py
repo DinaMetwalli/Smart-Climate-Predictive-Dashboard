27f7b797e9742413e2f45a54d8bfe7dc24a10a55
Dina Metwalli <dina.moh657@gmail.com>
2026-02-11 19:08:30 +0000

    refactor(data): remove file handling from data loader

--- data/preparation/data_loader.py
+++ data/preparation/data_loader.py
@@ -6,41 +6,19 @@ import xarray as xr
 from pathlib import Path
 
 class DataLoader():
-    def __init__(self, file_added: bool = True, file_type: str = None, file_name: str = None, regions:list = None):
-        self.file_added = file_added
-        self.file_type = file_type # To handle both NC and CSV/XLSX formats
-        self.file_name = file_name
+    def __init__(self, regions:list):
         self.regions_list = regions
         
         print("→ insitialized DataLoader ←")
 
-    def load_data(self) -> None:
+    def load_data(self, file = None) -> None:
         """
-        Dynamically loads the dataset depending on its type (File or API call)
+        Dynamically loads the dataset depending on its type (file upload or API call)
         """
-        if self.file_added:
-            BASE_DIR = Path.cwd().parent
-            file_path = BASE_DIR / "Smart-Climate-Predictive-Dashboard" / "data" / "sources" / "Datasets" / "NOAAGLOBALTEMP"
-            file = os.path.join(file_path, self.file_name)
-            print(file)
-
-            if os.path.exists(file):
-                if self.file_type == "nc":
-                    print("→ NC file type ←")
-                    df = xr.open_dataset(file)
-                    # To be done later...
-
-                elif self.file_type == "csv":
-                    print("→ CSV/XLSX file type ←")
-                    data = pd.read_csv(file)
-                    
-                    # Replace empty fields in csv with None
-                    data = data.replace(np.nan, None)
-                    return self.__process_file_data(data)
-            else:
-                raise Exception(f"'{self.file_name}' file was not found.")
-        else:
+        if not file:
             return self.__process_regional_api_data()
+        else:
+            return self.__process_file_data(file)
 
         
     def __process_file_data(self, df) -> pd.DataFrame:
@@ -53,10 +31,12 @@ class DataLoader():
         Returns:
             pd.DataFrame: the processed dataset.
         """
-        # Only works with CSV for now, will be modified later to support NC as well.
-        df.columns = ['Year', 'Anomaly']
+        
+        df = df.replace(np.nan, None)
+
+        df.columns = ['Date', 'Anomaly']
         df['Anomaly'] = df['Anomaly'].astype(float)
-        df = df.set_index('Year')
+        df = df.set_index('Date')
 
         print(df.head())
 
@@ -90,11 +70,11 @@ class DataLoader():
                 
                 # Extract fetched results to DataFrame
                 temp_df = pd.DataFrame.from_dict(data['data'], orient='index').reset_index()
-                temp_df.columns = ['Year', 'Anomaly']
+                temp_df.columns = ['Date', 'Anomaly']
                 temp_df['Anomaly'] = temp_df['Anomaly'].astype(float)
                 
                 temp_df['Region'] = region
-                temp_df = temp_df.set_index('Year')
+                temp_df = temp_df.set_index('Date')
                 
                 all_dfs.append(temp_df)
                 
