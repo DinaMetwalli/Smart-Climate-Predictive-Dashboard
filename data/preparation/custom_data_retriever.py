d69ae0e0862e2a75057ca81cb73b8aef80153a4d
Dina Metwalli <dina.moh657@gmail.com>
2026-02-11 16:58:56 +0000

    refactor(data): improve code separation and speed for custom uploads

--- data/preparation/custom_data_retriever.py
+++ data/preparation/custom_data_retriever.py
@@ -1,39 +1,39 @@
 import pandas as pd
 import xarray as xr
+import numpy as np
+
 from src.utils.errors import InvalidFileTypeError, FileProcessingError, IncompatibleDataError
 
 class CustomDatasetRetriever:
-    def __init__(self):
-        self.dataset = None
+    def __init__(self, file):
+        self.file = file
     
-    def load_dataset_from_file(self, file) -> None:
+    def load_dataset_from_file(self) -> None:
         
-        file_type = self.validate_file_type(file)
-        ds = self.validate_file_data(file, file_type)
+        file_type = self.validate_file_type()
+        ds = self.validate_columns(file_type)
+        self.validate_data_types(ds)
         
         validated_ds = self.validate_regions(ds)
 
         self.dataset = validated_ds
 
         return validated_ds
-
-    def get_dataset(self):
-        return self.dataset
     
-    def validate_file_type(self, file_name) -> str:
-        if file_name.endswith(".csv"):
+    def validate_file_type(self) -> str:
+        if self.file.filename.endswith(".csv"):
             return "csv"
-        elif file_name.endswith(".nc"):
+        elif self.file.filename.endswith(".nc"):
             return "nc"
         else:
             raise InvalidFileTypeError("Ivalid file type provided. Files must have CSV or NC extensions.")
 
-    def validate_file_data(self, file, file_type):
+    def validate_columns(self, file_type):
         try:    
             if file_type == "csv":
-                data = pd.read_csv(file)
+                data = pd.read_csv(self.file)
             else:
-                ds = xr.open_dataset(file)
+                ds = xr.open_dataset(self.file)
                 data = ds.to_dataframe().reset_index()
 
         except Exception:
@@ -46,24 +46,25 @@ class CustomDatasetRetriever:
                 "The provided dataset structure is incompatible. Please ensure the fields (Date, Anomaly, Region) are present in that order."
                 )
         
+        return data
+    
+    def validate_data_types(self, data):
         # Check all input data is of the expected type
-        if not data['Date'].map(lambda x: isinstance(x, int)).all():
+        if not np.issubdtype(data['Date'].dtype, np.integer):
             raise IncompatibleDataError("Provided column 'Date' contains non-numeric values. Expected format: YYYYMM")
         
-        if not data['Anomaly'].map(lambda x: isinstance(x, (int, float))).all():
+        if not np.issubdtype(data['Anomaly'].dtype, np.number):
             raise IncompatibleDataError("Provided column 'Anomaly' contains non-numeric values.")
         
-        if not data['Region'].map(lambda x: isinstance(x, str)).all():
+        if not pd.api.types.is_string_dtype(data['Region']):
             raise IncompatibleDataError("Provided column 'Region' contains non-text values. Allowed values: Africa, Asia," \
             " Europe, Northamerica, Southamerica, Oceania.")
-        
-        return data
     
     def validate_regions(self, ds):
         valid_regions = ["africa", "asia", "europe", "northAmerica", "southAmerica", "oceania"]
         ds_regions = ds['Region'].unique()
         
-        if ds_regions not in valid_regions:
+        if list(ds_regions) not in valid_regions:
             raise IncompatibleDataError("One or more of the given continents/regions are not recognised. Allowed values: Africa, Asia," \
             " Europe, Northamerica, Southamerica, Oceania.")
         
