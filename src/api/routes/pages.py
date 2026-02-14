0107a60a07edf5efa7c223d3d90301497941a165
Dina Metwalli <dina.moh657@gmail.com>
2026-02-14 11:28:57 +0000

    feat(api): add main application pages

---
+++ src/api/routes/pages.py
@@ -0,0 +1,15 @@
+from flask import Blueprint, render_template
+
+pages_bp = Blueprint("pages_bp", __name__)
+
+@pages_bp.route("/", methods=["GET"])
+def index():
+    return render_template("index.html")
+
+@pages_bp.route("/history", methods=["GET"])
+def load_history_page():
+    return render_template("history.html")
+
+@pages_bp.route("/about", methods=["GET"])
+def load_about_page():
+    return render_template("about.html")
