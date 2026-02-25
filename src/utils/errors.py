e608ab65208e266cc066001cbc849473631f4e56
Dina Metwalli <dina.moh657@gmail.com>
2026-02-25 10:25:36 +0000

    feat: add custom errors for user accounts

--- src/utils/errors.py
+++ src/utils/errors.py
@@ -19,3 +19,18 @@ class FileProcessingError(UserError):
 
 class FileTypeMismatchError(UserError):
     """Error for when different (supported) file types are uploaded."""
+
+class PasswordTooShortError(UserError):
+    """Error for when the password used for registration is less than 8 characters."""
+
+class UsernameTooShortError(UserError):
+    """Error for when the username used for registration is less than 4 characters."""
+
+class InvalidUsername(UserError):
+    """Error for when the username used for registration is invalid."""
+
+class UsernameAlreadyExistsError(UserError):
+    """Error for when a user already exists with the same username used for registraton."""
+
+class UserDoesNotExist(UserError):
+    """Error for when a requested user cannot be found."""
