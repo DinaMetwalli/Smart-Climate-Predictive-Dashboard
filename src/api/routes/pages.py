6d737dec8d9c266670dffc82bbf9190756547394
Dina Metwalli <dina.moh657@gmail.com>
2026-03-07 12:41:20 +0000

    feat(ui): register pages and correct links in login page

--- src/api/routes/pages.py
+++ src/api/routes/pages.py
@@ -6,10 +6,26 @@ pages_bp = Blueprint("pages_bp", __name__)
 def index():
     return render_template("index.html")
 
-@pages_bp.route("/live", methods=["GET"])
+@pages_bp.route("/upload", methods=["GET"])
+def load_upload_page():
+    return render_template("upload.html")
+
+@pages_bp.route("/charts", methods=["GET"])
+def load_charts_page():
+    return render_template("charts.html")
+
+@pages_bp.route("/history", methods=["GET"])
 def load_history_page():
-    return render_template("live.html")
+    return render_template("history.html")
 
 @pages_bp.route("/about", methods=["GET"])
 def load_about_page():
     return render_template("about.html")
+
+@pages_bp.route("/login", methods=["GET"])
+def load_login_page():
+    return render_template("login.html")
+
+@pages_bp.route("/register", methods=["GET"])
+def load_register_page():
+    return render_template("register.html")
