690778d9ff8ad080d163c8b9a1dd90244a9d84db
Dina Metwalli <dina.moh657@gmail.com>
2026-03-25 07:19:22 +0000

    fix(db): remove month index unique constraint in predictions table

--- src/init_sql/5_predictions.sql
+++ src/init_sql/5_predictions.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS predictions(
     id uuid NOT NULL DEFAULT gen_random_uuid() UNIQUE,
-    month_index integer NOT NULL UNIQUE,
-    prediction_val decimal(5,5) NOT NULL,
+    month_index integer NOT NULL,
+    prediction_val numeric(8,3) NOT NULL,
     analysis_id uuid NOT NULL,
     region_id uuid NOT NULL,
     PRIMARY KEY (id),
